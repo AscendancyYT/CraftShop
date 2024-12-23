78ca9d96f4215c80b5e75c71ce77cb84bb0a4221
Single-Phantom <misterminecraftter@gmail.com>
2024-12-23 23:40:31 +0500

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,18 @@
 const farmCard = document.querySelector(".farms");
 const buildingCard = document.querySelector(".buildings");
 
-farmCard.addEventListener('click', function(){
-  window.location.href="./routes/farms.html"
-})
+let loader;
+
+
+farmCard.addEventListener("click", function () {
+  window.location.href = "./routes/farms.html";
+});
+
+function loadingFunc() {
+  loader = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.querySelector(".loader").style.display = "none";
+  document.querySelector(".container").style.display = "flex";
+}
